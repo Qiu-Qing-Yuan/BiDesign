8df2e627474646486c43652fc39877aae14b33d7
Qiu-Qingyuan <jiujiuweigong@outlook.com>
2024-01-03 16:33:14 +0800

    配置文件

--- SGBlog/sangeng-blog/src/main/java/com/sangeng/SanGengBlogApplication.java
+++ SGBlog/sangeng-blog/src/main/java/com/sangeng/SanGengBlogApplication.java
@@ -1,10 +1,21 @@
 package com.sangeng;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author QiuQingyuan
  * @version 1.0
  * @Description: TODO
  * @date 2024-01-03  16:11
  */
+@SpringBootApplication
+@MapperScan("com.sangeng.mapper")
 public class SanGengBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanGengBlogApplication.class,args);
+    }
+
 }
